b087e8807b0ec91ad7a025c558c43a1a09e597a1
SimulatedGravity <107592310+SimulatedGravity@users.noreply.github.com>
2026-03-02 21:31:59 +1300

    version 2

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,16 +1,49 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rb => gameObject.GetComponent<Rigidbody2D>();
+    Animator animator => GetComponent<Animator>();
+    bool isGrounded = false;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] Vector2 groundCheckDimensions;
+    [SerializeField] LayerMask platformLayer;
+    [SerializeField] float movementSpeed;
+
+    float horizontalInput;
+
+    public void OnJump()
     {
-        
+        if(isGrounded)
+        rb.linearVelocity = jumpForce * Vector2.up;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue vector)
     {
-        
+        horizontalInput = vector.Get<Vector2>().x;
+    }
+
+    private void Update()
+    {
+        CheckForGround();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * movementSpeed * Time.fixedDeltaTime, rb.linearVelocity.y);
+        animator.SetBool("walking", (Mathf.Abs(horizontalInput) > 0.1));
+    }
+
+    private void CheckForGround()
+    {
+        isGrounded = Physics2D.BoxCast(transform.position, groundCheckDimensions, 0f, -transform.up, 0.1f, platformLayer);
+        animator.SetBool("grounded", isGrounded);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, (Vector3)groundCheckDimensions);
     }
 }
